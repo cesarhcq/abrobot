339f2771abcb0f685c0c07d4fe57c8cea112844f
Alan Tavares <alan.am.tavares@gmail.com>
2019-06-04 22:28:17 -0300

    fix files

--- abrobot_control/src/motor_controller.ino
+++ abrobot_control/src/motor_controller.ino
@@ -38,45 +38,6 @@
 
 SabertoothSimplified ST;
 
-<<<<<<< HEAD
-geometry_msgs::Twist vel_ref;
-geometry_msgs::Vector3Stamped vel_encoder_robo;
-geometry_msgs::Point32 vel_kinematic_robo;
-
-//Right wheel
-int encoder0PinA_Right = 5;
-//int encoder0PinB_Right = 6;
-int encoderPinALast_Right= LOW;
-int encoder0Pos_Right = 1;
-float vel_Right = 0;
-
-//Left wheel
-int encoder0PinA_Left = 3;
-//int encoder0PinB_Left = 4;
-int encoderPinALast_Left = LOW;
-int encoder0Pos_Left = 1;
-float vel_Left = 0;
-
-//Position encoder Left
-int read_Left = LOW;
-float Delta_t_Left = 0;
-float Sum_t_Left = 0;
-float Sum_vel_Left = 0;
-float PreviusMillis_Left = 0;
-int cont_Left = 0;
-
-//Position encoder Right
-int read_Right = LOW;
-float Delta_t_Right = 0;
-float Sum_t_Right = 0;
-float Sum_vel_Right = 0;
-float PreviusMillis_Right = 0;
-int cont_Right = 0;
-
-//Controller variable
-float epx_Right = 0;
-float epx_Left = 0;
-=======
 char encoder[] = "/encoder";
 unsigned long lastMilli = 0;       // loop timing
 double vel_req1 = 0;
@@ -111,7 +72,6 @@ long encoder0PosAnt_Left = 0;
 volatile long encoder0Pos_Right = 0;
 long encoder0PosAnt_Right = 0;
 
->>>>>>> gunther_real
 
 //ROS Function - Angular and linear Velocity Desired
 void handle_cmd(const geometry_msgs::Twist& msg){
@@ -127,10 +87,6 @@ void handle_cmd(const geometry_msgs::Twist& msg){
   vel_req2 = ( (2*x) + (z*L) )/2;  // Right wheel
 }
 
-<<<<<<< HEAD
-// Robot Differential Drive Kinematic
-void kinematic(){
-=======
 // *********************************************
 
 ros::NodeHandle  nh;
@@ -141,7 +97,6 @@ geometry_msgs::Vector3Stamped vel_encoder_msg;
 ros::Publisher pub_encoder("/vel_encoder", &vel_encoder_msg);
 
 // *********************************************
->>>>>>> gunther_real
 
 void setup()
 {
@@ -188,90 +143,6 @@ void setup()
 
 }
 
-<<<<<<< HEAD
-ros::NodeHandle  nh;
-//Subscribers
-ros::Subscriber<geometry_msgs::Twist> sub_rasp("/cmd_vel", &velResp);
-//Publisher
-ros::Publisher pub_encoder("/vel_encoder", &vel_encoder_robo);
-
-// *********************************************
-//Left wheel control
-void RosController_Wheel_Left() {
-
-  //Call reference speed from kinematic
-  float w_left = vel_kinematic_robo.x + (vel_kinematic_robo.x*0.0672);
-
-  //Debug kinematic
-  //vel_encoder_robo.z = w_left;
-
-  //Tangential velocity measured by encoder sensor - Vel_Left
-  read_Left = digitalRead(encoder0PinA_Left);
-  if ((encoderPinALast_Left == LOW) && (read_Left == HIGH)) {
-    // if (digitalRead(encoder0PinB_Left) == HIGH) {
-      encoder0Pos_Left++;
-      // Time between encoder signals
-      Delta_t_Left = (millis() - PreviusMillis_Left) * 0.001;
-      PreviusMillis_Left = millis();
-
-      //Linear speed with respect to Theta = 10 degrees (encoder sensitivity) of wheel displacement of R = 7.5 cm radius.
-      float w = (10 * PI / 180) / (Delta_t_Left);
-      vel_Left = w*R;
-      Sum_vel_Left = Sum_vel_Left + vel_Left;
-
-      //Mean of velocity in 30 interations
-      cont_Left++;
-      if(cont_Left>30){
-        Sum_vel_Left = vel_Left;
-        encoder0Pos_Left = 1;
-        cont_Left = 0;
-      }
-
-    //}
-  }
-
-  encoderPinALast_Left = read_Left;
-  //Average speed of a wheel V_linear
-  float Media_Vl_encoder = Sum_vel_Left / encoder0Pos_Left;
-
-  //Publisher Encoder Debug
-  //vel_encoder_robo.vector.x = vel_Left;
-
-  //convert sinal to volt
-  float Vl_gain;
-
-  //V_linear controll erro = (cinematica - encoder)
-  float erro = abs(w_left) - Media_Vl_encoder;
-  //Proportional gain
-  float kp = 0.2;
-  //Integrative Gain
-  float ki = 0.0008;
-
-  if (abs(erro) > 0.01) {
-    //PID control
-    float u = (erro * kp) + ((erro + epx_Left) * ki);
-    //Integrator Cumulative Error
-    epx_Left = epx_Left + erro;
-
-    //Change the sinal of controll
-    if(w_left < 0){
-      u = u*(-1);
-    }
-
-    if(w_left == 0){
-      //Reset commands
-      Media_Vl_encoder = 0;
-      encoder0Pos_Left = 1;
-      Sum_vel_Left = 0;
-      epx_Left = 0;
-    }else{
-      //Speed saturation conversion
-      Vl_gain = round((127 * u)/0.6);
-    }
-
-    //Degug-ROS
-    //vel_encoder_robo.y = u;
-=======
 void loop()
 {
   nh.spinOnce();
@@ -283,7 +154,6 @@ void loop()
 
     //PWM_val1 = ((vel_req1*127)/(0.8));
     //PWM_val2 = ((vel_req2*127)/(0.8));
->>>>>>> gunther_real
 
     //Output Motor Left
     ST.motor(MOTOR_LEFT, PWM_val1);// vl
@@ -309,88 +179,6 @@ void getMotorData(unsigned long time)  {
   vel_act2 = filterRight(vel_right);
 }
 
-<<<<<<< HEAD
-//Left wheel control
-void RosController_Wheel_Right() {
-
-  //Call reference speed from kinematic
-  float w_right = vel_kinematic_robo.y;
-
-  //Debug kinematic
-  //vel_encoder_robo.z = w_right;// - (w_right*0.004);
-
-  //Tangential velocity measured by encoder sensor - Vel_Left
-  read_Right = digitalRead(encoder0PinA_Right);
-  if ((encoderPinALast_Right == LOW) && (read_Right == HIGH)) {
-    // if (digitalRead(encoder0PinB_Left) == HIGH) {
-      encoder0Pos_Right++;
-      // Time between encoder signals
-      Delta_t_Right = (millis() - PreviusMillis_Right) * 0.001;
-      PreviusMillis_Right = millis();
-
-      //Linear speed with respect to Theta = 10 degrees (encoder sensitivity) of wheel displacement of R = 7.5 cm radius.
-      float w = (10 * PI / 180) / (Delta_t_Right);
-      vel_Right = w*R;
-      Sum_vel_Right = Sum_vel_Right + vel_Right;
-
-      //Mean of velocity in 30 interations
-      cont_Right++;
-      if(cont_Right>30){
-        Sum_vel_Right = vel_Right;
-        encoder0Pos_Right = 1;
-        cont_Right = 0;
-      }
-
-    //}
-  }
-
-  encoderPinALast_Right = read_Right;
-  //Average speed of a wheel V_linear
-  float Media_Vr_encoder = Sum_vel_Right / encoder0Pos_Right;
-
-  //Publish velocity right
-  float w_right_encoder = vel_Right;
-
-  //Publisher Encoder Debug
-  //vel_encoder_robo.vector.y = vel_Right;
-
-  //convert sinal to volt
-  float Vl_gain;
-
-  //V_linear controll erro = (cinematica - encoder)
-  float erro = abs(w_right) - Media_Vr_encoder;
-  //Proportional gain
-  float kp = 0.2;
-  //Integrative Gain
-  float ki = 0.0008;
-
-  if (abs(erro) > 0.01) {
-    //PID control
-    float u = (erro * kp) + ((erro + epx_Right) * ki);
-    //Integrator Cumulative Error
-    epx_Right = epx_Right + erro;
-
-    //Change the sinal of controll
-    if(w_right < 0){
-      u = u*(-1);
-    }
-
-    if(w_right == 0){
-      //Reset commands
-      Media_Vr_encoder = 0;
-      encoder0Pos_Right = 1;
-      Sum_vel_Right = 0;
-      epx_Right = 0;
-    }else{
-      //Speed saturation conversion
-      Vl_gain = round((127 * u)/0.6);
-      //Vl_gain = constrain(Vl_gain,-127,127);
-     // vel_encoder_robo.vector.z = Vl_gain;
-    }
-
-    //Degug-ROS
-    //vel_encoder_robo.y = u;
-=======
 double filterLeft(double vel_left)  {
 
   Sum_vel_Left = Sum_vel_Left + vel_left;
@@ -405,7 +193,6 @@ double filterLeft(double vel_left)  {
 
   return Media;
 }
->>>>>>> gunther_real
 
 double filterRight(double vel_right)  {
 
@@ -422,42 +209,6 @@ double filterRight(double vel_right)  {
   return Media;
 }
 
-<<<<<<< HEAD
-void publishEncoder() {
-
-  if(vel_kinematic_robo.x < 0) vel_Left = vel_Left*(-1);
-  if(vel_kinematic_robo.y < 0) vel_Right = vel_Right*(-1);
-
-  vel_encoder_robo.header.stamp = nh.now();
-  // Using kinematic
-  vel_encoder_robo.vector.x = vel_kinematic_robo.x;
-  vel_encoder_robo.vector.y = vel_kinematic_robo.y;
-  // using Encoder
-  // vel_encoder_robo.vector.x = vel_Left;
-  // vel_encoder_robo.vector.y = vel_Right
-  //vel_encoder_robo.vector.z = 0;
-  pub_encoder.publish(&vel_encoder_robo);
-  nh.spinOnce();
-}
-
-// *********************************************
-
-void setup()
-{
-  SabertoothTXPinSerial.begin(9600); // This is the baud rate you chose with the DIP switches.
-  //Serial.begin(9600);
-  delay(5000);
-  pinMode (encoder0PinA_Right, INPUT);
-  //pinMode (encoder0PinB_Right, INPUT);
-  pinMode (encoder0PinA_Left, INPUT);
-  //pinMode (encoder0PinB_Left, INPUT);
-  
-// ROS Initialization with Publishers and Subscribers 
-  nh.initNode();
-  nh.subscribe(sub_rasp);
-  //nh.advertise(pub_kinematic);
-  nh.advertise(pub_encoder);
-=======
 // PID correction - Function
 
 int updatePid(int id, double targetValue, double currentValue) {
@@ -485,7 +236,6 @@ int updatePid(int id, double targetValue, double currentValue) {
 
   new_cmd = constrain( ((pidTerm*127)/(0.8)) , -127, 127 );
   //new_cmd = round((pidTerm*127)/(0.8));
->>>>>>> gunther_real
 
   //new_pwm = constrain(double(command)*MAX_RPM/4095.0 + pidTerm, -MAX_RPM, MAX_RPM);
   //new_cmd = 4095.0*new_pwm/MAX_RPM;
@@ -493,16 +243,6 @@ int updatePid(int id, double targetValue, double currentValue) {
   return int(new_cmd);
 }
 
-<<<<<<< HEAD
-void loop()
-{
-  nh.spinOnce();
-  kinematic();
-  RosController_Wheel_Left();
-  RosController_Wheel_Right();
-  publishEncoder();
-  delay(1);
-=======
 void publishVEL(unsigned long time) {
   vel_encoder_msg.header.stamp = nh.now();
   vel_encoder_msg.header.frame_id = encoder;
@@ -523,5 +263,4 @@ void encoder_Right() {
   int pwm_value_2 = pulseIn(PWM_2, HIGH);
   if ((pwm_value_1 - pwm_value_2) > 0) encoder0Pos_Right++;
   else encoder0Pos_Right--;
->>>>>>> gunther_real
 }
--- base_controller/src/base_controller.cpp
+++ base_controller/src/base_controller.cpp
@@ -6,20 +6,13 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Vector3Stamped.h>
-<<<<<<< HEAD
-=======
 #include <geometry_msgs/Vector3.h>
->>>>>>> gunther_real
 
 double L = 0.5; // distance between axes
 double R = 0.0775; // wheel radius 
 
 double encoder_left = 0;
 double encoder_right = 0;
-<<<<<<< HEAD
-double vx = 0;
-double vth = 0;
-=======
 double encoder_dt = 0;
 double gyro_x = 0.0;
 double gyro_y = 0.0;
@@ -29,33 +22,18 @@ double gyro_z = 0.0;
 double v_encoder = 0;
 double dth_encoder = 0;
 
->>>>>>> gunther_real
 ros::Time encoder_time;
 bool init = false;
 
 void handle_vel_encoder(const geometry_msgs::Vector3Stamped& encoder) {
-<<<<<<< HEAD
-  encoder_left = encoder.vector.y;
-  encoder_right = encoder.vector.x;
-  encoder_time = encoder.header.stamp;
-=======
   encoder_time = encoder.header.stamp;
   encoder_left = encoder.vector.x;
   encoder_right = encoder.vector.y;
   encoder_dt = encoder.vector.z;
->>>>>>> gunther_real
 
   //ROS_INFO("encoder_left %lf - encoder_right %lf", encoder.vector.x, encoder.vector.y);
 }
 
-<<<<<<< HEAD
-// Robot Differential Drive Reverse Kinematic
-void reverse_kinematics(){
-  vx = (encoder_left + encoder_right)/2;
-  vth = (encoder_left - encoder_right)/L;
-
-  ROS_INFO("kinematics - vx %lf - vth %lf", vx, vth);
-=======
 // Gyro Function from Smartphone
 void handle_gyro( const geometry_msgs::Vector3& gyro) {
   gyro_x = gyro.x;
@@ -69,22 +47,12 @@ void reverse_kinematics(){
   dth_encoder = (encoder_right - encoder_left)/L; // angular
 
   //ROS_INFO("reverse_kinematics - v_encoder %lf - dth_encoder %lf", v_encoder, dth_encoder);
->>>>>>> gunther_real
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "base_controller");
 
   ros::NodeHandle nh;
-<<<<<<< HEAD
-  ros::Publisher odom_pub = nh.advertise<nav_msgs::Odometry>("odom", 50);
-  ros::Subscriber sub = nh.subscribe("/vel_encoder", 100, handle_vel_encoder);
-  
-  // // Crete tf - base link and Odometry
-  tf::TransformBroadcaster baselink_broadcaster;
-  tf::TransformBroadcaster odom_broadcaster;
-
-=======
   ros::NodeHandle nh_private_("~");
   ros::Subscriber gyro_sub = nh.subscribe("gyro", 50, handle_gyro);
   ros::Subscriber sub = nh.subscribe("/vel_encoder", 100, handle_vel_encoder);
@@ -100,36 +68,24 @@ int main(int argc, char** argv){
   double vth = 0.0;
   double dth = 0.0;
 
->>>>>>> gunther_real
   double x = 0.0;
   double y = 0.0;
   double th = 0.0;
 
-<<<<<<< HEAD
-=======
   nh_private_.getParam("alpha", alpha);
   nh_private_.getParam("use_imu", use_imu);
 
->>>>>>> gunther_real
   ros::Time current_time, last_time;
   ros::Rate r(10.0);
 
   while(nh.ok()){
 
-<<<<<<< HEAD
-    ros::spinOnce(); // check for incoming messages
-
-    baselink_broadcaster.sendTransform(
-    tf::StampedTransform(
-      tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.1, 0.0, 0.2)),
-=======
     ros::spinOnce(); //check for incoming messages
 
     //set tf base_link and laser 
     baselink_broadcaster.sendTransform(
     tf::StampedTransform(
       tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.16, 0.0, 0.13)),
->>>>>>> gunther_real
       ros::Time::now(),"base_link", "laser"));
 
     if(!init){
@@ -140,17 +96,6 @@ int main(int argc, char** argv){
 
     }else if(init){
 
-<<<<<<< HEAD
-      reverse_kinematics();
-      current_time = encoder_time;
-
-        //compute odometry in a typical way given the velocities of the robot
-      double dt = (current_time - last_time).toSec();
-      double delta_x = ( vx * cos(th) ) * dt;
-      double delta_y = ( vx * sin(th) ) * dt;
-      double delta_th = vth * dt;
-
-=======
       reverse_kinematics(); // return v_encoder and dth_encoder
       current_time = encoder_time;
       //compute odometry in a typical way given the velocities of the robot
@@ -171,27 +116,17 @@ int main(int argc, char** argv){
       ROS_INFO("DEBUG - delta_x %lf - delta_y %lf - time: %lf", delta_x, delta_y, dt);
 
       // position and orientation
->>>>>>> gunther_real
       x += delta_x;
       y += delta_y;
       th += delta_th;
 
-<<<<<<< HEAD
-      //ROS_INFO("encoder_left %lf - encoder_right %lf - time: %lf", encoder_left, encoder_right, encoder_time.toSec());
-      //ROS_INFO("DEBUG - vx %lf - vth %lf", vx, vth);
-=======
       //ROS_INFO("encoder_left %lf - encoder_right %lf - time: %lf", encoder_left, encoder_right, dt);
       //ROS_INFO("DEBUG - v_encoder %lf - dth_encoder %lf", v_encoder, dth);
->>>>>>> gunther_real
 
       //since all odometry is 6DOF we'll need a quaternion created from yaw
       geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
 
-<<<<<<< HEAD
-        //first, we'll publish the transform over tf
-=======
       //first, we'll publish the transform over tf
->>>>>>> gunther_real
       geometry_msgs::TransformStamped odom_trans;
       odom_trans.header.stamp = current_time;
       odom_trans.header.frame_id = "odom";
@@ -202,83 +137,28 @@ int main(int argc, char** argv){
       odom_trans.transform.translation.z = 0.0;
       odom_trans.transform.rotation = odom_quat;
 
-<<<<<<< HEAD
-        //send the transform
-      odom_broadcaster.sendTransform(odom_trans);
-
-        //next, we'll publish the odometry message over ROS
-=======
       //send the transform
       odom_broadcaster.sendTransform(odom_trans);
 
       //next, we'll publish the odometry message over ROS
->>>>>>> gunther_real
       nav_msgs::Odometry odom;
       odom.header.stamp = current_time;
       odom.header.frame_id = "odom";
 
-<<<<<<< HEAD
-        //set the position
-=======
       //set the position
->>>>>>> gunther_real
       odom.pose.pose.position.x = x;
       odom.pose.pose.position.y = y;
       odom.pose.pose.position.z = 0.0;
       odom.pose.pose.orientation = odom_quat;
 
-<<<<<<< HEAD
-        //set the velocity
-=======
       vx = (dt == 0)?  0 : v_encoder;
       vth = (dt == 0)? 0 : dth;
 
       //set the velocity
->>>>>>> gunther_real
       odom.child_frame_id = "base_link";
       odom.twist.twist.linear.x = vx;
       odom.twist.twist.linear.y = 0;
       odom.twist.twist.linear.z = 0;
-<<<<<<< HEAD
-      odom.twist.twist.angular.z = 0;
-      odom.twist.twist.angular.z = 0;
-      odom.twist.twist.angular.z = vth;
-
-      if (encoder_left == 0 && encoder_right == 0){
-        odom.pose.covariance[0] = 1e-9;
-        odom.pose.covariance[7] = 1e-3;
-        odom.pose.covariance[8] = 1e-9;
-        odom.pose.covariance[14] = 1e6;
-        odom.pose.covariance[21] = 1e6;
-        odom.pose.covariance[28] = 1e6;
-        odom.pose.covariance[35] = 1e-9;
-        odom.twist.covariance[0] = 1e-9;
-        odom.twist.covariance[7] = 1e-3;
-        odom.twist.covariance[8] = 1e-9;
-        odom.twist.covariance[14] = 1e6;
-        odom.twist.covariance[21] = 1e6;
-        odom.twist.covariance[28] = 1e6;
-        odom.twist.covariance[35] = 1e-9;
-      }
-      else{
-        odom.pose.covariance[0] = 1e-3;
-        odom.pose.covariance[7] = 1e-3;
-        odom.pose.covariance[8] = 0.0;
-        odom.pose.covariance[14] = 1e6;
-        odom.pose.covariance[21] = 1e6;
-        odom.pose.covariance[28] = 1e6;
-        odom.pose.covariance[35] = 1e3;
-        odom.twist.covariance[0] = 1e-3;
-        odom.twist.covariance[7] = 1e-3;
-        odom.twist.covariance[8] = 0.0;
-        odom.twist.covariance[14] = 1e6;
-        odom.twist.covariance[21] = 1e6;
-        odom.twist.covariance[28] = 1e6;
-        odom.twist.covariance[35] = 1e3;
-      }
-
-        //publish the message
-=======
       odom.twist.twist.angular.x = 0;
       odom.twist.twist.angular.y = 0;
       odom.twist.twist.angular.z = vth;
@@ -314,7 +194,6 @@ int main(int argc, char** argv){
       // }
 
       //publish the message
->>>>>>> gunther_real
       odom_pub.publish(odom);
     }
       // update the time
